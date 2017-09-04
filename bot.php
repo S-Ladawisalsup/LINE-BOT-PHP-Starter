45025a0b1f0f03445af0312936fa71f65c78dd7a
Sarayuth <pandaball7@gmail.com>
2017-09-04 18:51:03 +0700

    Edit user's sticker send response.

--- bot.php
+++ bot.php
@@ -78,16 +78,16 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			// $messages = [
-			// 	'type' => 'image',
-			// 	'originalContentUrl' => $img_url,
-   //              'previewImageUrl' => $img_url
-			// ];
 			$messages = [
-				'type' => 'sticker',
-				'packageId' => '1',
-    			'stickerId' => '1'
+				'type' => 'image',
+				'originalContentUrl' => $img_url,
+                'previewImageUrl' => $img_url
 			];
+			// $messages = [
+			// 	'type' => 'sticker',
+			// 	'packageId' => '1',
+   //  			'stickerId' => '1'
+			// ];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
