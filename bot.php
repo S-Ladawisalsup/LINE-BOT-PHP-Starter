f420bd42d927c736cc0f2cc3171f4499abfe1b5e
Sarayuth <pandaball7@gmail.com>
2017-09-06 15:37:24 +0700

    Try to read file from text.

--- bot.php
+++ bot.php
@@ -176,7 +176,7 @@ if (!is_null($events['events'])) {
 
 						$messages = [						
 							'type' => 'text',
-							'text' => count($lines) . $vas
+							'text' => count($lines)
 						];	
 					}
 
