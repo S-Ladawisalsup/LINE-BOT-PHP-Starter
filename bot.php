2ccf93dd1f49955b660cbc712d37fdf739ffa5f4
Sarayuth <pandaball7@gmail.com>
2017-09-06 17:11:50 +0700

    Try to read file from text.

--- bot.php
+++ bot.php
@@ -1,5 +1,5 @@
 <?php
-
+header('Content-Type: text/html; charset=UTF-8');
 /************************************************************************************************************************************/
 /*** PHP Function Zone. ***/
 function startsWith($haystack, $needle) {
