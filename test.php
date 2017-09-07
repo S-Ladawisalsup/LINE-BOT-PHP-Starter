7d3261cbb7e473225f4186c16a048910486e4958
Sarayuth <pandaball7@gmail.com>
2017-09-07 13:19:55 +0700

    Try to test read textfile.

--- test.php
+++ test.php
@@ -1,24 +1,5 @@
 <?php
 
-// $data = file('greeting.txt');
+ $data = file('greeting.txt');
 
-// $data = iconv("ISO-8859-1","UTF-8", $data);
-
-// echo count($data);
-$str = file('greeting.txt');
-/* Detect character encoding with current detect_order */
-echo mb_detect_encoding($str);
-
-/* "auto" is expanded according to mbstring.language */
-echo mb_detect_encoding($str, "auto");
-
-/* Specify encoding_list character encoding by comma separated list */
-echo mb_detect_encoding($str, "JIS, eucjp-win, sjis-win");
-
-/* Use array to specify encoding_list  */
-$ary[] = "ASCII";
-$ary[] = "JIS";
-$ary[] = "EUC-JP";
-echo mb_detect_encoding($str, $ary);
-
-?>
+ echo count($data);
