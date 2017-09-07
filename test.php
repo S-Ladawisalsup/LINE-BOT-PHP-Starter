aad762e8c44f12e7763461d87b97815dce88787b
Sarayuth <pandaball7@gmail.com>
2017-09-07 18:30:45 +0700

    Update read array from textfile.

--- test.php
+++ test.php
@@ -5,5 +5,5 @@ $data = file('question.txt');
 foreach ($data as $key) {
 	$decodetalker = utf8_decode($key);
 	$encodetalker = utf8_encode($decodetalker);
-	echo $key . '/' . $decodetalker . '/' . $encodetalker;
+	echo $key . '/' . $decodetalker . '/' . $encodetalker . '<br>';
 }
