72968b1c11a7af7fb1f59ab269e4e61db5a8600a
Sarayuth <pandaball7@gmail.com>
2017-09-07 11:30:00 +0700

    Try to test read textfile.

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
 <?php
 
-header('Content-Type: text/html; charset=utf-8');
+//header('Content-Type: text/html; charset=utf-8');
 
 $array = file('greeting.txt');
-$text =  iconv_substr($array, 0,100, "UTF-8");
+$text =  iconv_substr($array, 0, 100, "UTF-8");
 
 foreach ($array as $item) {
 	$test .= $item . '/';
