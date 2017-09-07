b82ba8097a27800bc97002010f1281b617b95ff8
Sarayuth <pandaball7@gmail.com>
2017-09-07 18:20:48 +0700

    Update read array from textfile.

--- test.php
+++ test.php
@@ -1,5 +1,23 @@
 <?php
 
+function isUtf8($string) {
+    if (function_exists("mb_check_encoding") && is_callable("mb_check_encoding")) {
+        return mb_check_encoding($string, 'UTF8');
+    }
+
+    return preg_match('%^(?:
+          [\x09\x0A\x0D\x20-\x7E]            # ASCII
+        | [\xC2-\xDF][\x80-\xBF]             # non-overlong 2-byte
+        |  \xE0[\xA0-\xBF][\x80-\xBF]        # excluding overlongs
+        | [\xE1-\xEC\xEE\xEF][\x80-\xBF]{2}  # straight 3-byte
+        |  \xED[\x80-\x9F][\x80-\xBF]        # excluding surrogates
+        |  \xF0[\x90-\xBF][\x80-\xBF]{2}     # planes 1-3
+        | [\xF1-\xF3][\x80-\xBF]{3}          # planes 4-15
+        |  \xF4[\x80-\x8F][\x80-\xBF]{2}     # plane 16
+    )*$%xs', $string);
+
+} 
+
 $data = file('question.txt');
 
-echo gettype($data[0]) . '/' . gettype($data);
+echo isUtf8($data[0]);
