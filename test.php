bb6a52b7eab9683c266c259352bc21cccf75f6d5
Piyanart Sungnern <admpiyanart@tbh1_domain.local>
2017-09-08 10:19:20 +0700

    test code php.

--- test.php
+++ test.php
@@ -2,9 +2,8 @@
 
 $data = file('question.txt');
 $numindex = rand(0, (count($data) - 1));
-$building = $data[$numindex];
-echo $numindex;
+$building = $data[14];
 echo $building;
 $costr = strlen($building);
 echo $costr;
-echo substr($building,0,$costr - 3 );
+echo substr($building,0,$costr - 2 );
