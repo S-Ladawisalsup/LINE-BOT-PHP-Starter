08759a798eecb8ae925141b6d5a714a92ce890f1
Piyanart Sungnern <admpiyanart@tbh1_domain.local>
2017-09-08 16:00:15 +0700

    edit test code

--- test.php
+++ test.php
@@ -1,10 +1,11 @@
 <?php
 
 echo date('UTC+7').'Fini'. "<br>";
-$t= time('GMT+7');
+date_default_timezone_set("Asia/Bangkok");
+$t= time();
 echo $t . "<br>";
 echo(date("Y-m-d||H:i:sP",$t) . "<br>");
-$day = strtolower(substr(date('UTC+7'), 0, 3));
-$da=date('Thailand/Bangkok');
+$day = strtolower(substr(date(), 0, 3));
+$da=date();
 echo "da = " .$da."<br>";
 echo "day = " . $day;
