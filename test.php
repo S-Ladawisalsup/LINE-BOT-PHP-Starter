3b29283461f4463f1f7c4738b62da00a1ccb16cd
Piyanart Sungnern <admpiyanart@tbh1_domain.local>
2017-09-08 15:31:20 +0700

    edit test code

--- test.php
+++ test.php
@@ -1,3 +1,10 @@
 <?php
 
 echo date('l').'Fini';
+$t= time('GMT+7');
+echo $t . "<br>";
+echo(date("Y-m-d||H:i:sP",$t) . "<br>");
+$day = strtolower(substr(date('UTC+7'), 0, 3));
+$da=date('Thailand/Bangkok');
+echo "da = " .$da."<br>";
+echo "day = " . $day;
