94e00fbc9d9b4e77c300c551b538a00b402a4da4
Sarayuth <pandaball7@gmail.com>
2017-09-08 10:51:16 +0700

    Remove charset that don't need from array text file.

--- test.php
+++ test.php
@@ -1,10 +1,5 @@
 <?php
 
-$data = file('question.txt');
-$numindex = rand(0, (count($data) - 1));
-$building = $data[14];
-echo $building;
-$costr = strlen($building);
-echo $costr;
-echo substr($building,0,$costr - 1 );
-echo $costr-1;
+$question = file('question.txt');
+
+echo count($question);
