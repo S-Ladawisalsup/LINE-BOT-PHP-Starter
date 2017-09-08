0540f1a9e2d26fb3d26841d3b6c3a1c860df7e3c
Sarayuth <pandaball7@gmail.com>
2017-09-08 14:47:30 +0700

    Change condition by reading array text.

--- bot.php
+++ bot.php
@@ -61,14 +61,14 @@ function GetQuesion($text, $flag) {
 		case 'math':
 			$ismath = file('text/question.txt');
 			$question[] = null;
-			for ($i = 0; $i < 4; $i++) {
+			for ($i = 1; $i < 4; $i++) {
 				array_push($question, $ismath[$i]);
 			}
 			break;
 		case 'issqrt':
 			$issqrt = file('text/math.txt');
 			$question[] = null;
-			for ($i = 0; $i < 4; $i++) {
+			for ($i = 1; $i < 4; $i++) {
 				array_push($question, $issqrt[$i]);
 			}
 			break;
