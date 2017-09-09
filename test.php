44cb5453228af629613755ce18ae48f7de233f05
Piyanart Sungnern <admpiyanart@tbh1_domain.local>
2017-09-09 14:22:37 +0700

    edit test php

--- test.php
+++ test.php
@@ -1,11 +1,7 @@
 <?php
 
-echo date('UTC+7').'Fini'. "<br>";
-date_default_timezone_set("Asia/Bangkok");
-$t= time();
-echo $t . "<br>";
-echo(date("Y-m-d||H:i:sP",$t) . "<br>");
-$day = strtolower(date("D"));
-$da=date();
-echo "da = " .$da."<br>";
-echo "day = " . $day;
+$answer = file('text/answer.txt');
+$ts = count($answer);
+$building = $answer[2];
+$building = substr($building, 0, strlen($building)-1);
+echo $building;
