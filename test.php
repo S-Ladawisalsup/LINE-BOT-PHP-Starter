7a5849d0c91393349aeda27f217099fc421cb766
Piyanart Sungnern <admpiyanart@tbh1_domain.local>
2017-09-09 14:25:15 +0700

    edit test php

--- test.php
+++ test.php
@@ -1,7 +1,7 @@
 <?php
 
-$answer = file('text/answer.txt');
+$answer = file('text/question.txt');
 $ts = count($answer);
-$building = $answer[2];
+$building = $answer[1];
 $building = substr($building, 0, strlen($building)-1);
 echo $building;
