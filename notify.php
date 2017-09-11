04fbe162495cd39de7ccc02afae9376da53aa4fe
Sarayuth <pandaball7@gmail.com>
2017-09-11 11:18:40 +0700

    Test line bot notify.

--- notify.php
+++ notify.php
@@ -1,48 +1,27 @@
 <?php
 
-$Token = 'yJwRVXYNItJu2V8q54aUCpGjtMteCzNNsRTbf60pt9J'//$_GET["Token"];
-$message = 'Test Line Notify' //$_GET["message "];
-
-echo 'This line is state before call function';
-
-line_notify($Token, $message);
-
-echo 'This is for line notify page.';
-
-fucntion line_notify($Token, $message) {
-
-	$lineapi = $Token; // ใส่ token key ที่ได้มา
-	$mms =  trim($message); // ข้อความที่ต้องการส่ง
-
-	date_default_timezone_set("Asia/Bangkok");
-
-	$chOne = curl_init();
-
-	curl_setopt($chOne, CURLOPT_URL, "https://notify-api.line.me/api/notify");
-
-	// SSL USE
-	curl_setopt($chOne, CURLOPT_SSL_VERIFYHOST, 0);
-	curl_setopt($chOne, CURLOPT_SSL_VERIFYPEER, 0);
-	//POST
-	curl_setopt($chOne, CURLOPT_POST, 1);
-	curl_setopt($chOne, CURLOPT_POSTFIELDS, "message=$mms");
-	curl_setopt($chOne, CURLOPT_FOLLOWLOCATION, 1);
-
-	$headers = array('Content-type: application/x-www-form-urlencoded', 'Authorization: Bearer ' . $lineapi . '',);
-
-	curl_setopt($chOne, CURLOPT_HTTPHEADER, $headers);
-	curl_setopt($chOne, CURLOPT_RETURNTRANSFER, 1);
-
-	$result = curl_exec($chOne);
-
-	//Check error
-	if(curl_error($chOne)) {
-		echo 'error:' . curl_error($chOne);
-	}
-	else {
-		$result_ = json_decode($result, true);
-		echo "status : ".$result_['status']; 
-		echo "message : " . $result_['message'];
-	}
-	curl_close($chOne);  
+function notify_message($message, $token){
+	$queryData = array('message' => $message);
+	$queryData = http_build_query($queryData, '', '&');
+	$headerOptions = array( 
+         				'http' => array(
+			            'method' => 'POST',
+			            'header' => 'Content-Type: application/x-www-form-urlencoded\r\n'
+			                        . 'Authorization: Bearer ' . $token . '\r\n'
+			                        . 'Content-Length: ' . strlen($queryData) . '\r\n',
+			            'content' => $queryData
+			         ),
+ );
+	 $context = stream_context_create($headerOptions);
+	 $result = file_get_contents(LINE_API, FALSE, $context);
+	 $res = json_decode($result);
+	 return $res;
 }
+/************************************************************************************************************************************/
+define('LINE_API', 'https://notify-api.line.me/api/notify');
+ 
+$token = 'yJwRVXYNItJu2V8q54aUCpGjtMteCzNNsRTbf60pt9J'; 	//ใส่Token ที่copy เอาไว้
+$str = 'Hello'; 											//ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+ 
+$res = notify_message($str, $token);
+echo $res;
