badbb09fb402480b40982b87bb895b82d71a9df8
Sarayuth <pandaball7@gmail.com>
2017-09-11 10:00:53 +0700

    Test line bot notify.

--- test.php
+++ test.php
@@ -1,11 +1,26 @@
 <?php
 
-echo date('UTC+7').'Fini'. "<br>";
-date_default_timezone_set("Asia/Bangkok");
-$t= time();
-echo $t . "<br>";
-echo(date("Y-m-d||H:i:sP",$t) . "<br>");
-$day = strtolower(date("D"));
-$da=date();
-echo "da = " .$da."<br>";
-echo "day = " . $day;
+define('LINE_API', 'https://notify-api.line.me/api/notify');
+ 
+$token = 'yJwRVXYNItJu2V8q54aUCpGjtMteCzNNsRTbf60pt9J'; 	//ใส่Token ที่copy เอาไว้
+$str = 'Hello'; 											//ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+ 
+$res = notify_message($str, $token);
+print_r($res);
+function notify_message($message, $token){
+	$queryData = array('message' => $message);
+	$queryData = http_build_query($queryData, '', '&');
+	$headerOptions = array( 
+         				'http' => array(
+			            'method' => 'POST',
+			            'header' => 'Content-Type: application/x-www-form-urlencoded\r\n'
+			                        . 'Authorization: Bearer ' . $token . '\r\n'
+			                        . 'Content-Length: ' . strlen($queryData) . '\r\n',
+			            'content' => $queryData
+			         ),
+ );
+	 $context = stream_context_create($headerOptions);
+	 $result = file_get_contents(LINE_API, FALSE, $context);
+	 $res = json_decode($result);
+	 return $res;
+}
