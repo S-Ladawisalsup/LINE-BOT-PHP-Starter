ceca068d7843cbe5d7aa759a61a58cbc4820f86c
Piyanart Sungnern <admpiyanart@tbh1_domain.local>
2017-09-11 11:54:07 +0700

    edit test php

--- test.php
+++ test.php
@@ -1,13 +1,13 @@
 <?php
 $numstick = 0;
 $randpack = rand(1,2);
-if($randpack=1)
+if($randpack==1)
 {
 	$ra1 = rand(0,2);
-	if($ra1 = 0)
+	if($ra1 == 0)
 	{
 		$ra2 = rand(0,18);
-		if($ra2=18)
+		if($ra2==18)
 		{
 			$ra2 = 21;//
 			$numstick = $ra2;
@@ -17,13 +17,13 @@ if($randpack=1)
 			$numstick = $ra2;
 		}
 	}
-	else if($ra1 = 1)
+	else if($ra1 == 1)
 	{
 		$ra2 = rand(0,39);
 		$ra2 = $ra2+100;
 		$numstick = $ra2;
 	}
-	else if($ra1 = 2)
+	else if($ra1 ==2)
 	{
 		$ra2 = rand(1,30);
 		$ra2 = $ra2 + 400;
@@ -33,7 +33,7 @@ if($randpack=1)
 else
 {
 	$ra1 = rand(0,2);
-	if($ra1 = 0)
+	if($ra1 == 0)
 	{
 		$ra2 = rand(19,47);
 		if($ra2 <= 21)//
@@ -41,14 +41,18 @@ else
 			$ra2 = $ra2 - 1;
 			$numstick = $ra2;
 		}
+		else
+		{
+			$numstick = $ra2;
+		}
 	}
-	else if($ra1 = 1)
+	else if($ra1 == 1)
 	{
 		$ra2 = rand(40,79);
 		$ra2 = $ra2 +100;
 		$numstick = $ra2;
 	}
-	else if($ra1 = 2)
+	else if($ra1 == 2)
 	{
 		$ra2 = rand(1,27);
 		$ra2 = $ra2 + 500;
