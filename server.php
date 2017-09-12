895d0c567edc2cd35d273c44b87a3c51b6ca006b
Sarayuth <pandaball7@gmail.com>
2017-09-12 15:54:32 +0700

    Add PHP Http server.

--- server.php
+++ server.php
@@ -9,4 +9,20 @@ $read=socket_read($accept,1024) or die("Cannot read from socket");
 echo $read;
 socket_write($accept,"Hello client");
 socket_close($sock);
+
+/*****************************************************************************************************
+<?php
+$address="127.0.0.1";
+$port="3222";
+$msg="Hello server";
+
+$sock=socket_create(AF_INET,SOCK_STREAM,0) or die("Cannot create a socket");
+socket_connect($sock,$address,$port) or die("Could not connect to the socket");
+socket_write($sock,$msg);
+
+$read=socket_read($sock,1024);
+echo $read;
+socket_close($sock);
+?>
+*****************************************************************************************************/
 ?>
