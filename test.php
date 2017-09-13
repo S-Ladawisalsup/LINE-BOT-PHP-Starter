00b63ec52ddc62ac2a1949a161ceca72fc9ad907
Sarayuth <pandaball7@gmail.com>
2017-09-13 17:34:40 +0700

    Try to test translate.

--- test.php
+++ test.php
@@ -9,6 +9,6 @@ $translate = new TranslateClient();
 $result = $translate->translate($text, [
     'target' => $targetLanguage,
 ]);
-// print("Source language: $result[source]\n");
-// print("Translation: $result[text]\n");
-echo $result['source'] . '=>' . $result['text'];
+print("Source language: $result[source]\n");
+print("Translation: $result[text]\n");
+//echo $result['source'] . '=>' . $result['text'];
