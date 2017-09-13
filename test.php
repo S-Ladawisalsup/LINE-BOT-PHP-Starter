0a82db25aa45e693d487a008345283ec4e391421
Sarayuth <pandaball7@gmail.com>
2017-09-13 15:20:04 +0700

    Try to test translate.

--- test.php
+++ test.php
@@ -1,5 +1,13 @@
 <?php
-$str = '@123a E l e 34แปล pha 45n-=งะt//s';
+function translate($from_lan='en', $to_lan='th', $text){
+    $json = json_decode(file_get_contents('https://ajax.googleapis.com/ajax/services/language/translate?v=1.0&q=' . urlencode($text) . '&langpair=' . $from_lan . '|' . $to_lan));
+    $translated_text = $json->responseData->translatedText;
+
+    return $translated_text;
+}
+
+$str = '@123e l e 34แปล pha 45n-=งะt//s';
 $str = preg_replace('/[^A-Za-z]/', '', $str);
 $str = ucfirst(strtolower($str));
+$str = translate($str);
 echo $str;
