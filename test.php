fac94261f06f2e087940c5b9c6a6e2b5a4e5e111
Sarayuth <pandaball7@gmail.com>
2017-09-13 14:58:10 +0700

    Try to test translate.

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
 $str = '@123a E l e 34แปล pha 45n-=งะt//s';
-$str = preg_replace("/[^A-Za-z]/", "", $str);
-$str = ucfirst($str);
+$str = preg_replace('/[^A-Za-z]/', '', $str);
+$str = ucfirst(strtolower($str));
 echo $str;
