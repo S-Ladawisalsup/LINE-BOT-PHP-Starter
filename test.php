a061519b7aacc33053d24b4808609314ed59a5d2
Sarayuth <pandaball7@gmail.com>
2017-11-10 09:54:17 +0700

    Split function file(s) from main bot function.

--- test.php
+++ test.php
@@ -1,14 +1,3 @@
 <?php
 
-use Google\Cloud\Translate\TranslateClient;
-
-$text = 'Elephant';
-$targetLanguage = 'th';
-
-$translate = new TranslateClient();
-$result = $translate->translate($text, [
-    'target' => $targetLanguage,
-]);
-print("Source language: $result[source]\n");
-print("Translation: $result[text]\n");
-//echo $result['source'] . '=>' . $result['text'];
+echo 'ok';
