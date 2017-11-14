2381732d6f68c19f287194432d88ab96f1054d0c
Sarayuth <pandaball7@gmail.com>
2017-11-14 17:52:22 +0700

    Test read data form heroku postgresql database.

--- test.php
+++ test.php
@@ -1,5 +1,43 @@
 <?php
 
+// $dsn = 'pgsql:'
+// 		. 'host=ec2-54-243-187-133.compute-1.amazonaws.com;'
+// 		. 'dbname=dfusod038c3j35;'
+// 		. 'user=mmbbbssobrmqjs;'
+// 		. 'port=5432;'
+// 		. 'sslmode=require;'
+// 		. 'password=fc2027eb6a706cd190646863367705a7969cbd85c0a86eed7a67d0dc6976bffa';
+
+// $db = new PDO($dsn);
+
+// $query = 'SELECT id, ip_addr, serv_name, status, lastchangedatetime FROM tbhlinebotserv ORDER BY id ASC';
+// $result = $db->query($query);
+
+// echo '<table style="border: 1px solid black; border-collapse: collapse;">
+// 		<thead>
+// 			<tr>
+// 				<th style="border: 1px solid black; border-collapse: collapse;">ID</th>
+// 				<th style="border: 1px solid black; border-collapse: collapse;">IP Address</th>
+// 				<th style="border: 1px solid black; border-collapse: collapse;">Server Name</th>
+// 				<th style="border: 1px solid black; border-collapse: collapse;">Server Status</th>
+// 				<th style="border: 1px solid black; border-collapse: collapse;">Last Active Time</th>
+// 			</tr>
+// 		</thead>
+// 		<tbody>';
+
+// while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+//     echo '<tr>';
+//     echo '<td style="border: 1px solid black; border-collapse: collapse;">' . $row["id"] . '</td>';
+//     echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["ip_addr"]) . '</td>';
+//     echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["serv_name"]) . '</td>';
+//     echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["status"]) . '</td>';
+//     echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["lastchangedatetime"]) . '</td>';
+//     echo '</tr>';
+// }
+// $result->closeCursor();
+
+// echo "</tbody></table>";
+
 $dsn = 'pgsql:'
 		. 'host=ec2-54-243-187-133.compute-1.amazonaws.com;'
 		. 'dbname=dfusod038c3j35;'
@@ -10,17 +48,16 @@ $dsn = 'pgsql:'
 
 $db = new PDO($dsn);
 
-$query = 'SELECT id, ip_addr, serv_name, status, lastchangedatetime FROM tbhlinebotserv ORDER BY id ASC';
+$query = 'SELECT id, questiontext, questiontype, typename FROM tbhlinebotchkqa ORDER BY id ASC';
 $result = $db->query($query);
 
 echo '<table style="border: 1px solid black; border-collapse: collapse;">
 		<thead>
 			<tr>
 				<th style="border: 1px solid black; border-collapse: collapse;">ID</th>
-				<th style="border: 1px solid black; border-collapse: collapse;">IP Address</th>
-				<th style="border: 1px solid black; border-collapse: collapse;">Server Name</th>
-				<th style="border: 1px solid black; border-collapse: collapse;">Server Status</th>
-				<th style="border: 1px solid black; border-collapse: collapse;">Last Active Time</th>
+				<th style="border: 1px solid black; border-collapse: collapse;">คำถาม</th>
+				<th style="border: 1px solid black; border-collapse: collapse;">ชนิด</th>
+				<th style="border: 1px solid black; border-collapse: collapse;">ประเภท</th>
 			</tr>
 		</thead>
 		<tbody>';
@@ -28,10 +65,9 @@ echo '<table style="border: 1px solid black; border-collapse: collapse;">
 while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
     echo '<tr>';
     echo '<td style="border: 1px solid black; border-collapse: collapse;">' . $row["id"] . '</td>';
-    echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["ip_addr"]) . '</td>';
-    echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["serv_name"]) . '</td>';
-    echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["status"]) . '</td>';
-    echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["lastchangedatetime"]) . '</td>';
+    echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["questiontext"]) . '</td>';
+    echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["questiontype"]) . '</td>';
+    echo '<td style="border: 1px solid black; border-collapse: collapse;">' . htmlspecialchars($row["typename"]) . '</td>';
     echo '</tr>';
 }
 $result->closeCursor();
