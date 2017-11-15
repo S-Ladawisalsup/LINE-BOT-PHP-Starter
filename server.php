0552ebd2b2ec6c3c51b0e82ae24ef0361764a662
Sarayuth <pandaball7@gmail.com>
2017-11-15 17:25:16 +0700

    Test debug write data to database.

--- server.php
+++ server.php
@@ -1,24 +1,27 @@
 <?php
 
-echo "Temperature is " . $_POST["temperature"] . ' at time ' . $_POST["timestamp"] . '<br />';
+// echo "Temperature is " . $_POST["temperature"] . ' at time ' . $_POST["timestamp"] . '<br />';
 
 // Get POST body content
 $jsons = file_get_contents('php://input');
 // Parse JSON
 $datas = json_decode($jsons, true);
 
-echo '1. data is ' . $datas["timestamp"] . '<br />';
-echo '2. data is ' . $_POST["timestamp"] . '<br />';
-echo '3. data is ' . $datas["data"] . '<br />';
-echo '4. data is ' . $_POST["data"] . '<br />';
-echo '5. json is ' . $jsons . '<br />';
-echo '6. datas is ' . $datas . '<br />';
+// echo '1. data is ' . $datas["timestamp"] . '<br />';
+// echo '2. data is ' . $_POST["timestamp"] . '<br />';
+// echo '3. data is ' . $datas["data"] . '<br />';
+// echo '4. data is ' . $_POST["data"] . '<br />';
+// echo '5. json is ' . $jsons . '<br />';
+// echo '6. datas is ' . $datas . '<br />';
  
 
-// //if (!is_null($datas['events'])) {
+if (!is_null($datas['events'])) {
 // foreach ($datas["data"] as $data) {
-// 	echo 'Time is : ' . $datas["timestamp"] . '<br />';
-// 	echo 'Name is : ' . $data["name"] . '<br />';
-// 	echo 'res is : '. $data["name"] . '<br />';
-// 	echo 'data is : '. $data["data"] . '<br />';
-// }
+	echo 'Time is : ' . $datas["timestamp"] . '<br />';
+	echo 'Name is : ' . $datas["data"]["name"] . '<br />';
+	echo 'res is : '. $datas["data"]["res"] . '<br />';
+	echo 'data is : '. $datas["data"]["data"] . '<br />';
+}
+else {
+	echo "Cannot receive any json data";
+}
