a91c427004fd5ebf538a5ed3df10bd179a98839a
Sarayuth <pandaball7@gmail.com>
2017-11-15 15:15:02 +0700

    Test debug write data to database.

--- server.php
+++ server.php
@@ -1,16 +1,16 @@
 <?php
 
-//echo "Temperature is " . $_POST["temperature"] . ' at time ' . $_POST["timestamp"];
+echo "Temperature is " . $_POST["temperature"] . ' at time ' . $_POST["timestamp"];
 
-// Get POST body content
-$jsons = file_get_contents('php://input');
-// Parse JSON
-$datas = json_decode($jsons, true);
+// // Get POST body content
+// $jsons = file_get_contents('php://input');
+// // Parse JSON
+// $datas = json_decode($jsons, true);
 
-//if (!is_null($datas['events'])) {
-foreach ($datas["data"] as $data) {
-	echo 'Time is : ' . $datas["timestamp"] . '<br />';
-	echo 'Name is : ' . $data["name"] . '<br />';
-	echo 'res is : '. $data["name"] . '<br />';
-	echo 'data is : '. $data["data"] . '<br />';
-}
+// //if (!is_null($datas['events'])) {
+// foreach ($datas["data"] as $data) {
+// 	echo 'Time is : ' . $datas["timestamp"] . '<br />';
+// 	echo 'Name is : ' . $data["name"] . '<br />';
+// 	echo 'res is : '. $data["name"] . '<br />';
+// 	echo 'data is : '. $data["data"] . '<br />';
+// }
