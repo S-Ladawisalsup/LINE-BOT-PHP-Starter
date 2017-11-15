55c7cc3c1f616a92a280d88a117271344dfcb6e2
Sarayuth <pandaball7@gmail.com>
2017-11-15 16:15:25 +0700

    Test debug write data to database.

--- server.php
+++ server.php
@@ -7,7 +7,9 @@ $jsons = file_get_contents('php://input');
 // Parse JSON
 $datas = json_decode($jsons, true);
 
-echo 'data is ' . $datas;
+echo '1. data is ' . $datas["timestamp"] . '<br />';
+echo '2. data is ' . $_POST["timestamp"] . '<br />';
+echo '3. data is ' . $datas["data"] . '<br />';
 
 // //if (!is_null($datas['events'])) {
 // foreach ($datas["data"] as $data) {
