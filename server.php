4388c1396b9cff378c86566946e1318ad0345544
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-16 16:49:30 +0700

    Debug receive data from php page.

--- server.php
+++ server.php
@@ -1,8 +1,8 @@
 <?php
 
 if (!is_null($_POST["temperature"])) {
-	header("Refresh:0");
-	echo "Temperature is " . $_POST["temperature"];// . ' at time ' . $_POST["timestamp"];
+	//header("Refresh:0");
+	echo "Temperature is " . $_POST["temperature"];
 }
 else {
 	echo "Cannot receive any temperature data";
