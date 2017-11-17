ac181aea3ede7bd8fd5581ee2a0180213119d1f9
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-17 15:12:29 +0700

    Debug insert into database from php code.

--- test.php
+++ test.php
@@ -74,7 +74,7 @@
 // }
 
 date_default_timezone_set("Asia/Bangkok");
-echo "status 7000 ok " . date("Y-m-d H:i:s") . '<br />';
+echo "status 700 ok";
 // echo "Test sending form post (temperature=26)";
 
 // echo '<br /><form method="post" action="https://cryptic-harbor-32168.herokuapp.com/server.php">
