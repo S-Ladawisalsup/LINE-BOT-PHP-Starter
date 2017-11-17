0c6396784c64bbf9625c13339523d3d4f7c520cd
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-17 13:23:38 +0700

    Debug test writing data to postgresql database.

--- test.php
+++ test.php
@@ -74,10 +74,10 @@
 // }
 
 date_default_timezone_set("Asia/Bangkok");
-//echo "status 200 ok " . date("Y-m-d H:i:s") . '<br />';
-echo "Test sending form post (temperature=26)";
+echo "status 200 ok " . date("Y-m-d H:i:s") . '<br />';
+// echo "Test sending form post (temperature=26)";
 
-echo '<br /><form method="post" action="https://cryptic-harbor-32168.herokuapp.com/server.php">
-				<input type="hidden" name="temperature" value="26" />
-				<input type="submit" value="send value 26" />
-			</form>';
+// echo '<br /><form method="post" action="https://cryptic-harbor-32168.herokuapp.com/server.php">
+// 				<input type="hidden" name="temperature" value="26" />
+// 				<input type="submit" value="send value 26" />
+// 			</form>';
