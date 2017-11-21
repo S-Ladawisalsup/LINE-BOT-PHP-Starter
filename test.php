b3b7ea19e39967cb52a1b4d76a912742f4d6fbf1
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-21 12:56:55 +0700

    Debug query data and show in timestamp type.

--- test.php
+++ test.php
@@ -1,14 +1,14 @@
 <?php
 
-// $dsn = 'pgsql:'
-// 	. 'host=ec2-54-243-187-133.compute-1.amazonaws.com;'
-// 	. 'dbname=dfusod038c3j35;'
-// 	. 'user=mmbbbssobrmqjs;'
-// 	. 'port=5432;'
-// 	. 'sslmode=require;'
-// 	. 'password=fc2027eb6a706cd190646863367705a7969cbd85c0a86eed7a67d0dc6976bffa';
+$dsn = 'pgsql:'
+	. 'host=ec2-54-243-187-133.compute-1.amazonaws.com;'
+	. 'dbname=dfusod038c3j35;'
+	. 'user=mmbbbssobrmqjs;'
+	. 'port=5432;'
+	. 'sslmode=require;'
+	. 'password=fc2027eb6a706cd190646863367705a7969cbd85c0a86eed7a67d0dc6976bffa';
 
-// $db = new PDO($dsn);
+$db = new PDO($dsn);
 
 $one = 1;
 
@@ -21,6 +21,6 @@ $one = 1;
 // // }
 // // $results->closeCursor();
 
-echo $one . '<br />' . ' ' . $db;
+echo $one . '<br />';
 
 echo $last_temp['temp'] . "C at " . $last_temp['datetime'];
