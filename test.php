dadb2ecefff68fd65d2867efc2b9bc9cb95d77de
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-21 13:23:06 +0700

    Debug query data and show in timestamp type.

--- test.php
+++ test.php
@@ -14,22 +14,22 @@ $db = new PDO($dsn);
 
 $query_locnametemp = 'SELECT id, temperature, lastchangedatetime FROM tbhlinebottemploc ORDER BY id ASC';
 $results = $db->query($query_locnametemp);
-// $last_temp = array();
-// $index = 0;
-// while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-//     $last_temp[$index] = array();
-//     $last_temp[$index]['id'] = $row["id"];
-// 	$last_temp[$index]['temp'] = htmlspecialchars($row["temperature"]);
-// 	$last_temp[$index]['datetime'] = htmlspecialchars($row["lastchangedatetime"]);
-// 	$index = $index + 1;
-// }
+$last_temp = array();
+$index = 0;
+while ($row = $results->fetch(PDO::FETCH_ASSOC)) {
+    $last_temp[$index] = array();
+    $last_temp[$index]['id'] = $row["id"];
+	$last_temp[$index]['temp'] = htmlspecialchars($row["temperature"]);
+	$last_temp[$index]['datetime'] = htmlspecialchars($row["lastchangedatetime"]);
+	$index = $index + 1;
+}
 $results->closeCursor();
 
-// foreach ($last_temp as $final_temp) {
-// 	if ($final_temp['id'] == $one) {
-// 		$temp_new = array('temp'     => $final_temp['temp'], 
-// 						  'datetime' => $final_temp['datetime']);
-// 	}
-// }
+foreach ($last_temp as $final_temp) {
+	if ($final_temp['id'] == $one) {
+		$temp_new = array('temp'     => $final_temp['temp'], 
+						  'datetime' => $final_temp['datetime']);
+	}
+}
 
 echo $temp_new['temp'] . "C at " . $temp_new['datetime'];
