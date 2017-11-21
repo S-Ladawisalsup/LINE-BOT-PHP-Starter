cd9e3ef64371edb572a430be1ec5d1f81daebb35
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-21 11:51:14 +0700

    Debug query data and show in timestamp type.

--- test.php
+++ test.php
@@ -1,26 +1,26 @@
 <?php
 
-$dsn = 'pgsql:'
-	. 'host=ec2-54-243-187-133.compute-1.amazonaws.com;'
-	. 'dbname=dfusod038c3j35;'
-	. 'user=mmbbbssobrmqjs;'
-	. 'port=5432;'
-	. 'sslmode=require;'
-	. 'password=fc2027eb6a706cd190646863367705a7969cbd85c0a86eed7a67d0dc6976bffa';
+// $dsn = 'pgsql:'
+// 	. 'host=ec2-54-243-187-133.compute-1.amazonaws.com;'
+// 	. 'dbname=dfusod038c3j35;'
+// 	. 'user=mmbbbssobrmqjs;'
+// 	. 'port=5432;'
+// 	. 'sslmode=require;'
+// 	. 'password=fc2027eb6a706cd190646863367705a7969cbd85c0a86eed7a67d0dc6976bffa';
 
-$db = new PDO($dsn);
+// $db = new PDO($dsn);
 
-$one = 1;
+// $one = 1;
 
-// $query_locnametemp = 'SELECT temperature, lastchangedatetime FROM tbhlinebottemploc WHERE id = $one';
-// $results = $db->query($query_locnametemp);
-// while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
-//     $last_temp = array();
-// 	$last_temp['temp'] = htmlspecialchars($row["temperature"]);
-// 	$last_temp['datetime'] = htmlspecialchars($row["lastchangedatetime"]);
-// }
-// $results->closeCursor();
+// // $query_locnametemp = 'SELECT temperature, lastchangedatetime FROM tbhlinebottemploc WHERE id = $one';
+// // $results = $db->query($query_locnametemp);
+// // while ($row = $result->fetch(PDO::FETCH_ASSOC)) {
+// //     $last_temp = array();
+// // 	$last_temp['temp'] = htmlspecialchars($row["temperature"]);
+// // 	$last_temp['datetime'] = htmlspecialchars($row["lastchangedatetime"]);
+// // }
+// // $results->closeCursor();
 
-echo $one . ' ' . $db;
+// echo $one . ' ' . $db;
 
 echo $last_temp['temp'] . "C at " . $last_temp['datetime'];
