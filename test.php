3d3c7105ffaccb95d8239a1ffde3f1e6dc8ef765
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-21 15:32:30 +0700

    Debug bot answer temperature by reading database.

--- test.php
+++ test.php
@@ -55,7 +55,7 @@ if ($curr_place != 0) {
 	}
 	else {
 		//lastchangedatetime != datenow, tell date and time
-		$last_temp['datetime'] = date("Y-m-d H:i");
+		$last_temp['datetime'] = date("Y-m-d H:i", strtotime($last_temp['datetime']));
 		// $previous_date = date("d/m/Y", strtotime(substr($last_temp['datetime'], 0, 10)));
 		// $previous_time = substr($last_temp['datetime'], 11);
 		// $previous_time = str_replace(':', '.', $previous_time);
