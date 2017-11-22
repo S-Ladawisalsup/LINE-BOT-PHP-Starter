54f62ef03025d568c0e9e9661e4ca0cb12348e3e
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-22 15:39:40 +0700

    Debug php server recieve json data.

--- test.php
+++ test.php
@@ -1,3 +1,26 @@
 <?php
 
-echo "ok";
+$state = true;
+
+if ($state) {
+	$curr_temperature = 30;
+	$location = 'ITSD Room';
+	UpdateTempToDB($curr_temperature, $location);
+	$state = false;
+}
+
+echo "<br />Update successful";
+
+function UpdateTempToDB($curr_temperature, $location) {
+	$db = pg_connect("host=ec2-54-243-187-133.compute-1.amazonaws.com 
+					port=5432 
+					dbname=dfusod038c3j35 
+					user=mmbbbssobrmqjs 
+					password=fc2027eb6a706cd190646863367705a7969cbd85c0a86eed7a67d0dc6976bffa");
+
+	echo 'temperature is ' . $curr_temperature . ' in ' . $location;
+
+	$result = pg_query($db, "UPDATE tbhlinebottemploc 
+							SET temperature = $curr_temperature
+							WHERE location = $location");				
+}
