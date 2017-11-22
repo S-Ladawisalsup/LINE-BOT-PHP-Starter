10b0f5fd8f8362f3cc8d536d625ac70e7210dc5c
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-22 14:13:13 +0700

    Debug php server recieve json data.

--- test.php
+++ test.php
@@ -1,15 +1,3 @@
 <?php
-// test_req = {
-// 				"temperature": data_obj1["data"], 
-// 			 	"server":[
-// 			 			 {
-// 			  				"name":"192.1.100.13", 
-// 			  				"status":"True"
-// 			  			 }, 
-// 					     {
-// 					    	"name":"192.1.100.14", 
-// 					    	"status":"True"
-// 					     }
-// 			    	  	 ]
-// 			}
-echo "200";
+
+echo "ok";
