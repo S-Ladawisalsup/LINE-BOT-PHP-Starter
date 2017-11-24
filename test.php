0a399ad0f2d62f9e15e4a6c76b25a6b5be5b75a0
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-24 16:24:07 +0700

    Try bot to start send message.

--- test.php
+++ test.php
@@ -13,7 +13,7 @@ foreach ($ggezwp as $key) {
 	echo '<td style="border: 1px solid black; border-collapse: collapse;">' . $key['type'] . '</td></tr>';
 }		
 echo '</tbody></table><br />';
-
+//id = Ca35db1d5c584c6467d717df89a0302ec (g)
 
 function QuestionWordFromDBTB() {
 	$dsn = 'pgsql:'
@@ -29,7 +29,7 @@ function QuestionWordFromDBTB() {
 	$t = 'text';
 
 	//$query = "SELECT questiontext, questiontype FROM tbhlinebotwmode WHERE questiontype = '1' OR questiontype = '7'";
-	$query = "SELECT $t, type FROM tbhlinebotans WHERE type = '12'";
+	$query = "SELECT $t, type FROM tbhlinebotans WHERE type = '11'";
 	$result = $db->query($query);
 
 	$words = array();
