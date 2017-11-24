1907a576cf9ff9a55d9703506f232ac92e82f2cf
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-24 14:58:10 +0700

    Add timer mode answer into database.

--- test.php
+++ test.php
@@ -29,7 +29,7 @@ function QuestionWordFromDBTB() {
 	$t = 'text';
 
 	//$query = "SELECT questiontext, questiontype FROM tbhlinebotwmode WHERE questiontype = '8'";//" OR questiontype = '8'";
-	$query = "SELECT $t, type FROM tbhlinebotans WHERE type = '10'";
+	$query = "SELECT $t, type FROM tbhlinebotans WHERE type = '12'";
 	$result = $db->query($query);
 
 	$words = array();
