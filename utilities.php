b17ca1395a98a399da465137c6727dc1e13b5de1
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-24 15:14:05 +0700

    Add timer mode answer into database.

--- utilities.php
+++ utilities.php
@@ -393,7 +393,7 @@ function InsertDataToDB() {
 	// 						,('Alaina', 'Alaina ซอย รัชดาภิเษก 7 ถนนรัชดาภิเษก แขวง ดินแดง เขต ดินแดง กรุงเทพมหานคร 10400', '13.772270', '100.573075')
 	// 						;");//		
 
-	//now id 34-39 is empty
+	//now tbhlinebotwmode id 35-39 is empty
 	$t = 'text';
 	$result = pg_query($db, "INSERT INTO tbhlinebotwmode (id, questiontext, questiontype) VALUES 
 						(34, 'อยู่ไหน', '3')						
