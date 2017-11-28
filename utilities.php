72bcef72d5ca6150da1202758a4b243bfe364c22
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-11-28 16:23:21 +0700

    Add update server name function to textfile web service.

--- utilities.php
+++ utilities.php
@@ -389,13 +389,13 @@ function GetLocation() {
 }
 /**********************************************************************************************************************************/
 function updateserver() {
-	// $dsn = 'pgsql:'
-	// 	. 'host=ec2-54-243-187-133.compute-1.amazonaws.com;'
-	// 	. 'dbname=dfusod038c3j35;'
-	// 	. 'user=mmbbbssobrmqjs;'
-	// 	. 'port=5432;'
-	// 	. 'sslmode=require;'
-	// 	. 'password=fc2027eb6a706cd190646863367705a7969cbd85c0a86eed7a67d0dc6976bffa';
+	$dsn = 'pgsql:'
+		. 'host=ec2-54-243-187-133.compute-1.amazonaws.com;'
+		. 'dbname=dfusod038c3j35;'
+		. 'user=mmbbbssobrmqjs;'
+		. 'port=5432;'
+		. 'sslmode=require;'
+		. 'password=fc2027eb6a706cd190646863367705a7969cbd85c0a86eed7a67d0dc6976bffa';
 	$db = new PDO($dsn);
 	$query = 'SELECT ip_addr FROM tbhlinebotserv ORDER BY id ASC'; 
 	$result = $db->query($query);
