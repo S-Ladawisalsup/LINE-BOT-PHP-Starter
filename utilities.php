47f2dc03e9e26471f59195ed42378532607f514a
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-12-06 14:00:22 +0700

    Debug Line Chat Bot register mode.

--- utilities.php
+++ utilities.php
@@ -515,7 +515,8 @@ function RegisterMode($text, $userId, $userType) {
 			}
 			else {
 				$error = true;
-				$str = "ก็บอกให้กรอกวันที่ในรูปแบบ(ค.ศ.) dd/mm/yyyy เช่น 01/01/1900 ไง ไปเริ่มกรอกใหม่ตั้งแต่ต้นเลยไป๊!";
+				//$str = "ก็บอกให้กรอกวันที่ในรูปแบบ(ค.ศ.) dd/mm/yyyy เช่น 01/01/1900 ไง ไปเริ่มกรอกใหม่ตั้งแต่ต้นเลยไป๊!";
+				$str = count($matches);
 			}
 			break;
 		case '5':
