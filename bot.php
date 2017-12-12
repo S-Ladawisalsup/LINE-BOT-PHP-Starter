d1dc24159e726fe3ee1a7f832838821ee6370fd0
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-12-12 17:02:15 +0700

    Test message type template bot return.

--- bot.php
+++ bot.php
@@ -182,18 +182,22 @@ if (!is_null($events['events'])) {
 									if (strpos($text, 'testmsgbyball') !== false) {
 										//InsertDataToDB();
 										$messages = [
-											'type' => 'template',
-											'altText' => 'this is a button template',
-											'template' => array('type' => 'buttons', 
-															    'thumbnailImageUrl' => 'https://cryptic-harbor-32168.herokuapp.com/images/tbh_icon.png',
-															    'imageAspectRatio' => 'rectangle', 
-															    'imageSize' => 'cover',
-															    'imageBackgroundColor' => '#FFFFFF',
-															    'title' => 'Menu',
-															    'text' => 'Please Select',
-															    'actions' => array('type' => 'uri',
-															    				   'label' => 'Go to manual',
-															    				   'uri' => 'https://cryptic-harbor-32168.herokuapp.com/manual.html'))
+											'type'     => 'template',
+											'altText'  => 'this is a button template',
+											'template' => [
+															'type' 				   => 'buttons', 
+															'thumbnailImageUrl'    => 'https://cryptic-harbor-32168.herokuapp.com/images/tbh_icon.png',
+															'imageAspectRatio' 	   => 'rectangle', 
+															'imageSize' 		   => 'cover',
+															'imageBackgroundColor' => '#FFFFFF',
+															'title' 			   => 'Menu',
+															'text' 				   => 'Please Select',
+															'actions' 			   => [
+																						'type'  => 'uri',
+																						'label' => 'Go to manual',
+																						'uri'   => 'https://cryptic-harbor-32168.herokuapp.com/manual.html'
+															]
+											]
 										];
 									}
 									//--------------------------------------------------------
