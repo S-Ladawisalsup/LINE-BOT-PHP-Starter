ca2d22ae91b68851db0ad039e81267495fee5db1
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-12-12 11:24:14 +0700

    Fixed list on waitng register's user.

--- bot.php
+++ bot.php
@@ -42,6 +42,11 @@ if (!is_null($events['events'])) {
 								//RegisterMode($text, $event['source'][$event['source']['type'] . 'Id'], $event['source']['type'])
 							];
 						}
+						$messages = [						
+							'type' => 'text',
+							'text' => $text . "\n" . $event['source'][$event['source']['type'] . 'Id'] . "\n" . $event['source']['type']
+							//RegisterMode($text, $event['source'][$event['source']['type'] . 'Id'], $event['source']['type'])
+						];
 						break;
 					case 'allow':
 						// Compare message calling bot's name
