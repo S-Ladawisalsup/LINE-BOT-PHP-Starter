9d29b5ec7066113f734ef90c48a6e27dfb4e721b
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-12-15 10:00:37 +0700

    Try bot reply template confrim type message.

--- bot.php
+++ bot.php
@@ -183,15 +183,7 @@ if (!is_null($events['events'])) {
 									// Test case to insert data to postgresql database.
 									if (strpos($text, 'testmsgbyball') !== false) {
 										//InsertDataToDB();
-										$policies = file('text/policy.txt');
-										$tx = '';
-										foreach ($policies as $policy) {
-											$tx .= $policy;
-										}
-										$messages = [						
-											'type' => 'text',
-											'text' => $tx
-										]; 
+										PostbackDate($event['replyToken']);
 									}
 									//--------------------------------------------------------
 									else if (strpos($text, 'เปิดโหมดลงทะเบียนเข้าใช้งาน') !== false) {
