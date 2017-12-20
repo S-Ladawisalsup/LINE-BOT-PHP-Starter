0a39fe60270b3fad3464982b198df60a33e2fdbf
sarayuth ladawisalsup <sarayuth.la@TBH.THONBURIHOSPITAL.COM>
2017-12-20 12:57:55 +0700

    Test bot mode joking question.

--- test.php
+++ test.php
@@ -26,7 +26,7 @@ function QuestionWordFromDBTB() {
 
 	$t = 'text';
 
-	$query = "SELECT questiontext, questiontype FROM tbhlinebotwmode WHERE questiontype = '4'";
+	$query = "SELECT questiontext, questiontype FROM tbhlinebotwmode WHERE questiontype = '5'";
 	//$query = "SELECT $t, type FROM tbhlinebotans WHERE type = '13'";
 	//$query = "SELECT question, answer FROM tbhlinebotjokeq ORDER BY id DESC";
 	$result = $db->query($query);
